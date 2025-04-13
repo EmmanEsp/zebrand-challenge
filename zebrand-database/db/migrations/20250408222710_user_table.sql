23bab30cb2b0132a43c209beeb667f2eac46b3aa
EmmanEsp <emmanesp13@gmail.com>
2025-04-12 23:28:07 -0700

    organized user admin and inserted product items

--- zebrand-database/db/migrations/20250408222710_user_table.sql
+++ zebrand-database/db/migrations/20250408222710_user_table.sql
@@ -11,16 +11,5 @@ CREATE TABLE users (
     deleted_at TIMESTAMP WITH TIME ZONE
 );
 
-INSERT INTO users (name, role, email, password, created_at, updated_at, deleted_at)
-VALUES (
-    'string',
-    'admin',
-    'string@gmail.com',
-    '$argon2id$v=19$m=65536,t=3,p=4$e3ySTacZNvWYycldUVjeTw$SgORFB4piQPpbl0qIJQZrwztK0YdgIj5yAXM3hIcIPo',
-    '2025-04-09 21:52:01.44675+00',
-    NULL,
-    NULL
-);
-
 -- migrate:down
 DROP TABLE IF EXISTS users;
